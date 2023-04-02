9c9456016a4acbb10c7827d352a311221d587dd3
GitHub Action <action@github.com>
2023-04-02 13:39:23 +0000

    Commit healing

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,13 @@
+import express, { Application, Request, Response } from 'express';
+
 const app: Application = express();
 
 const port: number = 3001;
 
-app.get('/toto', (req: Request, res: Response) => {
-	res.send('Hello toto');
+app.get('/toto', (req: Request, res: Response) => { 
+  res.send('Hello toto'); 
 });
 
 app.listen(port, () => {
-	console.log(`App is listening on port ${port} !`);
+  console.log(`App is listening on port ${port} !`); 
 });
