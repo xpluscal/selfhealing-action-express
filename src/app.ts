989402572eb40dd6badece726eb2630f9c643697
Calvin Hoenes <calvinhoenes@gmail.com>
2023-04-02 16:50:27 +1000

    added error

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,11 @@
-// const app: Application = express()
+const app: Application = express()
 
-// const port: number = 3001
+const port: number = 3001
 
-// app.get('/toto', (req: Request, res: Response) => {
-//     res.send('Hello toto')
-// })
+app.get('/toto', (req: Request, res: Response) => {
+    res.send('Hello toto')
+})
 
-// app.listen(port, function () {
-//     console.log(`App is listening on port ${port} !`)
-// })
+app.listen(port, function () {
+    console.log(`App is listening on port ${port} !`)
+})
